94386efa8a8ad2f1148379e50e54e3cd83eebbcb
Alina Romashenko <Romas-A@users.noreply.github.com>
2021-05-02 00:48:13 +0700

    Updated chessviz.h

--- src/lib/chessviz.h
+++ src/lib/chessviz.h
@@ -1,13 +1,13 @@
 #pragma once
 struct Figura;
-void doska_print(int& sum_hod);
-void doska_default(int& sum_hod);
+void doska_print(int &sum_hod);
+void doska_default(int &sum_hod);
 void doska_figura_remove(int x, int y);
-void Ppawn_hod(int x, int y, int& x1, int& y1);
-void Rrook_hod(int x, int y, int& x1, int& y1);
-void Nknight_hod(int x, int y, int& x1, int& y1);
-void Bbishop_hod(int x, int y, int& x1, int& y1);
-void Qqueen_hod(int x, int y, int& x1, int& y1);
-void Kking_hod(int x, int y, int& x1, int& y1);
-void mamba(int x, int y, int& x1, int& y1);
-void doska_hod(int x, int y, int& x1, int& y1, int& sum_hod);
+bool Ppawn_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+bool Rrook_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+bool Nknight_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+bool Bbishop_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+bool Qqueen_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+void Kking_hod(int x, int y, int &x1, int &y1, bool &flag_buff);
+int mamba(int x, int y, int &x1, int &y1);
+void doska_hod(int x, int y, int &x1, int &y1, int &sum_hod, bool &flag_buff);
