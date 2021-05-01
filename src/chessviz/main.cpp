b7df11cd1b9ded3d458216085c7badc088e7868a
Alina Romashenko <Romas-A@users.noreply.github.com>
2021-05-02 00:47:12 +0700

    Updated main.cpp

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -11,8 +11,9 @@ int main()
     int str = 0;
     char stb = 0;
     int x1, y1;
+    bool flag_buff = false;
     while (cin >> stb >> str) {
-        doska_hod(stb, str, x1, y1, sum_hod);
+        doska_hod(stb, str, x1, y1, sum_hod, flag_buff);
     }
     return 0;
-}
+
